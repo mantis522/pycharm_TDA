176ccfc4361c9d33826d0f0d9fec1b2b7a1688de
mantis <2020203938@hanyang.ac.kr>
2020-07-08 18:47:04 +0900

    tiny

--- Learning/val_split_removed.py
+++ Learning/val_split_removed.py
@@ -136,7 +136,7 @@ model.add(Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
 
-hist = model.fit(x_train, y_train, epochs=8, batch_size=64, validation_data=(x_val, y_val)
+hist = model.fit(x_train, y_train, epochs=5, batch_size=64, validation_data=(x_val, y_val)
                     ,verbose=1)
 
 loss_and_metrics = model.evaluate(x_test, y_test, batch_size=64)
