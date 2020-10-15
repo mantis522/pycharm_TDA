5c7c7362dc6c493ce054e34cb60a65f58de60d7e
mantis <2020203938@hanyang.ac.kr>
2020-10-15 18:05:20 +0900

    tiny

--- Twitter_US/test.py
+++ Twitter_US/test.py
@@ -2,10 +2,18 @@ import pandas as pd
 import json
 import re
 
+
 test = "D:/ruin/data/Twitter_US_Airline/original/airline_tweet_test.csv"
+test_lab = "D:/data/17_742210_compressed_Tweets.csv/Tweets.csv"
 
-df = pd.read_csv(test)
+df = pd.read_csv(test_lab)
 df_sample = df[['airline_sentiment', 'text']]
-df_sample.rename(columns={"airline_sentiment":"변경후"}, inplace=True)
+# df_sample.rename(columns={"airline_sentiment":"변경후"}, inplace=True)
 
-print(df_sample)
+print(df_sample['airline_sentiment'])
+
+count = 0
+for a in range(len(df_sample)):
+    if df_sample['airline_sentiment'].values() == 'neutral':
+        count = count + 1
+print(count)
