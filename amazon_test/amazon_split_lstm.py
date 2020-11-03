612da72e267b7c43333c77da45f99a2557c77b15
mantis <2020203938@hanyang.ac.kr>
2020-11-03 18:12:54 +0900

    amazon_dataset_start

--- amazon_test/amazon_split_lstm.py
+++ amazon_test/amazon_split_lstm.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import json
-import matplotlib.pyplot as plt
 import os
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import to_categorical
@@ -16,59 +15,18 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.preprocessing import sequence
 import math
+from matplotlib import pyplot as plt
 
-home_test_dir = "D:/ruin/data/Twitter_US_Airline/original/airline_tweet_test.csv"
-home_train_dir = "D:/ruin/data/Twitter_US_Airline/original/airline_tweet_train.csv"
-
-home_removed_amod_neg = "D:/ruin/data/Twitter_US_Airline/removed/removed_amod_neg.json"
-home_removed_amod_neu = "D:/ruin/data/Twitter_US_Airline/removed/removed_amod_neu.json"
-home_removed_amod_pos = "D:/ruin/data/Twitter_US_Airline/removed/removed_amod_pos.json"
-home_removed_PP_neg = "D:/ruin/data/Twitter_US_Airline/removed/removed_PP_neg.json"
-home_removed_PP_neu = "D:/ruin/data/Twitter_US_Airline/removed/removed_PP_neu.json"
-home_removed_PP_pos = "D:/ruin/data/Twitter_US_Airline/removed/removed_PP_pos.json"
-home_removed_SBAR_neg = "D:/ruin/data/Twitter_US_Airline/removed/removed_SBAR_neg.json"
-home_removed_SBAR_neu = "D:/ruin/data/Twitter_US_Airline/removed/removed_SBAR_neu.json"
-home_removed_SBAR_pos = "D:/ruin/data/Twitter_US_Airline/removed/removed_SBAR_pos.json"
-
-test_dir = "D:/data/csv_file/airline_tweet_test.csv"
-train_dir = "D:/data/csv_file/airline_tweet_train.csv"
+train_dir = "D:/data/csv_file/amazon/amazon_100000.csv"
+test_dir = "D:/data/csv_file/amazon/amazon_test.csv"
 glove_100_dir = "D:/data/glove.6B/glove.6B.100d.txt"
 
-def making_df(file_directory, label):
-    with open(file_directory) as json_file:
-        json_data = json.load(json_file)
-
-    removed_sentence = json_data['removed_sentence']
-    removed_sentence = removed_sentence[0]
-    removed = []
-
-    for a in range(len(removed_sentence)):
-        for b in range(len(removed_sentence[a])):
-            removed.append(removed_sentence[a][b])
-
-    df = pd.DataFrame(removed)
-    df.columns = ['text']
-    df['label'] = label
-
-    return df
-
-def original_df(file_directory):
-    df = pd.read_csv(file_directory)
-    df = df[['airline_sentiment', 'text']]
-    df['airline_sentiment'] = df['airline_sentiment'].replace('negative', 0)
-    df['airline_sentiment'] = df['airline_sentiment'].replace('neutral', 1)
-    df['airline_sentiment'] = df['airline_sentiment'].replace('positive', 2)
-    df.rename(columns={"airline_sentiment": "label"}, inplace=True)
-    # df.rename(columns={"text": "data"}, inplace=True)
-
-    return df
-
-original_train_df = original_df(train_dir)
-original_test_df = original_df(test_dir)
+original_train_df = pd.read_csv(train_dir)
+original_test_df = pd.read_csv(test_dir)
 
 original_test_df, original_val_df = train_test_split(original_test_df, test_size=0.4, random_state=0)
 
-x = original_train_df['text']
+x = original_train_df['review']
 y = original_train_df['label']
 
 t = Tokenizer()
@@ -77,39 +35,35 @@ t.fit_on_texts(x)
 vocab_size = len(t.word_index) + 1
 sequences = t.texts_to_sequences(x)
 
-def max_tweet():
+def max_text():
     for i in range(1, len(sequences)):
         max_length = len(sequences[0])
         if len(sequences[i]) > max_length:
             max_length = len(sequences[i])
     return max_length
 
-tweet_num = max_tweet()
-maxlen = tweet_num
+text_num = max_text()
+maxlen = text_num
 
 padded_X = pad_sequences(sequences, padding='post', maxlen=maxlen)
 
-# labels = to_categorical(np.asarray(y))
-
-
-x_train = original_train_df['text'].values
+x_train = original_train_df['review'].values
 x_train = t.texts_to_sequences(x_train)
 x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
 y_train = original_train_df['label'].values
 y_train = to_categorical(np.asarray(y_train))
 
-x_test = original_test_df['text'].values
+x_test = original_test_df['review'].values
 x_test = t.texts_to_sequences(x_test)
 x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
 y_test = original_test_df['label'].values
 y_test = to_categorical(np.asarray(y_test))
 
-x_val = original_val_df['text'].values
+x_val = original_val_df['review'].values
 x_val = t.texts_to_sequences(x_val)
 x_val = sequence.pad_sequences(x_val, maxlen=maxlen)
 y_val = original_val_df['label'].values
 y_val = to_categorical(np.asarray(y_val))
-
 
 print('X_train size:', x_train.shape)
 print('y_train size:', y_train.shape)
@@ -138,7 +92,7 @@ for word, i in t.word_index.items():  # dictionary
         embedding_matrix[i] = embedding_vector # each row of matrix
 
 embedding_layer = Embedding(input_dim=vocab_size, output_dim=100, weights=[embedding_matrix],
-                           input_length = tweet_num, trainable=False)
+                           input_length = text_num, trainable=False)
 
 es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=4)
 mc = ModelCheckpoint('best_model.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
@@ -146,14 +100,28 @@ mc = ModelCheckpoint('best_model.h5', monitor='val_acc', mode='max', verbose=1, 
 model = Sequential()
 model.add(embedding_layer)
 model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
-model.add(Dense(3, activation='softmax'))
+model.add(Dense(2, activation='softmax'))
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 model.summary()
 
-hist_1 = model.fit(x_train, y_train, validation_data=(x_val, y_val), epochs=100, batch_size=64, verbose=1, callbacks=[es, mc])
+hist = model.fit(x_train, y_train, validation_data=(x_val, y_val), epochs=100, batch_size=64, verbose=1, callbacks=[es, mc])
 
 print("Accuracy...")
 loss, accuracy = model.evaluate(x_train, y_train, verbose=1)
 print("Training Accuracy: {:.4f}".format(accuracy))
 loss, accuracy = model.evaluate(x_test, y_test, verbose=1)
 print("Testing Accuracy:  {:.4f}".format(accuracy))
+
+# fig, loss_ax = plt.subplots()
+# acc_ax = loss_ax.twinx()
+
+# loss_ax.plot(hist.history['loss'], 'y', label='train loss')
+# loss_ax.plot(hist.history['val_loss'], 'r', label='val loss')
+# acc_ax.plot(hist.history['accuracy'], 'b', label='train acc')
+# acc_ax.plot(hist.history['val_accuracy'], 'g', label='val acc')
+# loss_ax.set_xlabel('epoch')
+# loss_ax.set_ylabel('loss')
+# acc_ax.set_ylabel('accuracy')
+# loss_ax.legend(loc='upper left')
+# acc_ax.legend(loc='lower left')
+# plt.show()
---
+++ amazon_test/split_data.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import json
+
+data_train = pd.read_csv("D:/data/amazon_review_polarity_csv/train.csv", names=['label', 'title', 'review'])
+data_train.loc[data_train["label"] == 1, "label"] = 0
+data_train.loc[data_train['label'] == 2, "label"] = 1
+data_train.drop(['title'], axis='columns', inplace=True)
+
+data_test = pd.read_csv("D:/data/amazon_review_polarity_csv/test.csv", names=['label', 'title', 'review'])
+data_test.loc[data_test["label"] == 1, "label"] = 0
+data_test.loc[data_test["label"] == 2, "label"] = 1
+data_test.drop(['title'], axis='columns', inplace=True)
+
+def split_df(label, number):
+    review_list = []
+    label_list = []
+    for a in range(len(data_test)):
+        if data_train['label'][a] == label:
+            label_list.append(data_train['label'][a])
+            review_list.append(data_train['review'][a])
+        if len(review_list) == number:
+            break
+    df = pd.DataFrame()
+    df['label'] = label_list
+    df['review'] = review_list
+
+    return df
+
+neg_df = split_df(0, 12500)
+pos_df = split_df(1, 12500)
+
+concat_train_df = pd.concat([neg_df, pos_df]).reset_index(drop=True)
+print(len(concat_train_df))
+
+concat_train_df.to_csv("D:/data/csv_file/amazon/amazon_test.csv", index=False)
