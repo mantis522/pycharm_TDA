c2527d51803407c0f8131fbf7fd4045c1962a9b9
mantis <2020203938@hanyang.ac.kr>
2021-01-07 17:59:24 +0900

    아마 2021년 첫 commit

--- popcorn_imdb/ratio_keras_lstm_aug.py
+++ popcorn_imdb/ratio_keras_lstm_aug.py
@@ -9,20 +9,20 @@ from tensorflow.keras.preprocessing import sequence
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import to_categorical
 
-original_train_neg_df = pd.read_csv(r"D:\data\csv_file\popcorn\popcorn_5_neg.csv")
-original_train_pos_df = pd.read_csv(r"D:\data\csv_file\popcorn\popcorn_5_pos.csv")
+original_train_neg_df = pd.read_csv(r"D:\data\csv_file\popcorn\popcorn_10_neg.csv")
+original_train_pos_df = pd.read_csv(r"D:\data\csv_file\popcorn\popcorn_10_pos.csv")
 original_concat_df = pd.concat([original_train_neg_df, original_train_pos_df]).reset_index(drop=True)
 original_test_df = pd.read_csv(r"D:\data\word2vec-nlp-tutorial\test.csv", names=['id', 'label', 'review'])
 original_test_df = original_test_df.drop(original_test_df.index[0]).reset_index(drop=True)
 del original_test_df['id']
 del original_concat_df['Unnamed: 0']
 
-removed_amod_neg = r"D:\data\json_data\removed_data\popcorn\ratio\5\removed_popcorn_5_amod_neg.json"
-removed_amod_pos = r"D:\data\json_data\removed_data\popcorn\ratio\5\removed_popcorn_5_amod_pos.json"
-removed_PP_neg = r"D:\data\json_data\removed_data\popcorn\ratio\5\removed_popcorn_5_PP_neg.json"
-removed_PP_pos = r"D:\data\json_data\removed_data\popcorn\ratio\5\removed_popcorn_5_PP_pos.json"
-removed_SBAR_neg = r"D:\data\json_data\removed_data\popcorn\ratio\5\removed_popcorn_5_SBAR_neg.json"
-removed_SBAR_pos = r"D:\data\json_data\removed_data\popcorn\ratio\5\removed_popcorn_5_SBAR_pos.json"
+removed_amod_neg = r"D:\data\json_data\removed_data\popcorn\ratio\10\removed_popcorn_10_amod_neg.json"
+removed_amod_pos = r"D:\data\json_data\removed_data\popcorn\ratio\10\removed_popcorn_10_amod_pos.json"
+removed_PP_neg = r"D:\data\json_data\removed_data\popcorn\ratio\10\removed_popcorn_10_PP_neg.json"
+removed_PP_pos = r"D:\data\json_data\removed_data\popcorn\ratio\10\removed_popcorn_10_PP_pos.json"
+removed_SBAR_neg = r"D:\data\json_data\removed_data\popcorn\ratio\10\removed_popcorn_10_SBAR_neg.json"
+removed_SBAR_pos = r"D:\data\json_data\removed_data\popcorn\ratio\10\removed_popcorn_10_SBAR_pos.json"
 
 def making_df(file_directory, label):
     with open(file_directory) as json_file:
@@ -67,7 +67,7 @@ removed_PP_neg = making_df(removed_PP_neg, 0)
 removed_SBAR_neg = making_df(removed_SBAR_neg, 0)
 removed_SBAR_pos = making_df(removed_SBAR_pos, 1)
 
-concat_train_df = pd.concat([original_concat_df, removed_SBAR_pos, removed_SBAR_neg]).reset_index(drop=True)
+concat_train_df = pd.concat([original_concat_df,removed_SBAR_pos, removed_SBAR_neg, removed_PP_pos, removed_PP_neg]).reset_index(drop=True)
 
 original_test_df, original_val_df = train_test_split(original_test_df, test_size=0.4, random_state=0)
 
@@ -113,7 +113,7 @@ mc = ModelCheckpoint('best_model.h5', monitor='val_acc', mode='max', verbose=2, 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 print('Train...')
-hist = model.fit(x_train, y_train, validation_data=(x_val, y_val), epochs=3, batch_size=64, verbose=1, callbacks=[es, mc])
+hist = model.fit(x_train, y_train, validation_data=(x_val, y_val), epochs=1, batch_size=64, verbose=1, callbacks=[es, mc])
 score, acc = model.evaluate(x_test, y_test, batch_size=64, verbose=1)
 print('Test score : ', score)
 print('Test accuracy : ', acc)
--- test/test.py
+++ test/test.py
@@ -1,1 +1,65 @@
-test = 0
+import json
+import pandas as pd
+
+
+original_train_neg_df = pd.read_csv(r"D:\data\csv_file\popcorn\popcorn_10_neg.csv")
+original_train_pos_df = pd.read_csv(r"D:\data\csv_file\popcorn\popcorn_10_pos.csv")
+original_concat_df = pd.concat([original_train_neg_df, original_train_pos_df]).reset_index(drop=True)
+original_test_df = pd.read_csv(r"D:\data\word2vec-nlp-tutorial\test.csv", names=['id', 'label', 'review'])
+original_test_df = original_test_df.drop(original_test_df.index[0]).reset_index(drop=True)
+del original_test_df['id']
+del original_concat_df['Unnamed: 0']
+
+removed_amod_neg = r"D:\data\json_data\removed_data\popcorn\ratio\10\removed_popcorn_10_amod_neg.json"
+removed_amod_pos = r"D:\data\json_data\removed_data\popcorn\ratio\10\removed_popcorn_10_amod_pos.json"
+removed_PP_neg = r"D:\data\json_data\removed_data\popcorn\ratio\10\removed_popcorn_10_PP_neg.json"
+removed_PP_pos = r"D:\data\json_data\removed_data\popcorn\ratio\10\removed_popcorn_10_PP_pos.json"
+removed_SBAR_neg = r"D:\data\json_data\removed_data\popcorn\ratio\10\removed_popcorn_10_SBAR_neg.json"
+removed_SBAR_pos = r"D:\data\json_data\removed_data\popcorn\ratio\10\removed_popcorn_10_SBAR_pos.json"
+
+def making_df(file_directory, label):
+    with open(file_directory) as json_file:
+        json_data = json.load(json_file)
+
+    removed_sentence = json_data['removed_sentence']
+    removed_sentence = removed_sentence[0]
+    removed = []
+
+    for a in range(len(removed_sentence)):
+        for b in range(len(removed_sentence[a])):
+            removed.append(removed_sentence[a][b])
+
+    df = pd.DataFrame(removed)
+    df.columns = ['review']
+    df['label'] = label
+
+    return df
+
+def making_test_df(file_directory, label):
+    with open(file_directory) as json_file:
+        json_data = json.load(json_file)
+
+    removed_sentence = json_data['removed_sentence']
+    removed_sentence = removed_sentence[0]
+    removed = []
+
+    for a in range(len(removed_sentence)):
+        for b in range(len(removed_sentence[a])):
+            removed.append(removed_sentence[a][b])
+
+    df = pd.DataFrame(removed)
+    df.columns = ['review']
+    df['label'] = label
+
+    return df
+
+removed_amod_pos = making_df(removed_amod_pos, 1)
+removed_amod_neg = making_df(removed_amod_neg, 0)
+removed_PP_pos = making_df(removed_PP_pos, 1)
+removed_PP_neg = making_df(removed_PP_neg, 0)
+removed_SBAR_neg = making_df(removed_SBAR_neg, 0)
+removed_SBAR_pos = making_df(removed_SBAR_pos, 1)
+
+concat_train_df = pd.concat([original_concat_df, removed_PP_neg, removed_PP_pos]).reset_index(drop=True)
+
+print(concat_train_df)
